d02bf797d8800b7999dffef25ee868ab5b1cddb9
Anton Trafimovich <tonytrof@gmail.com>
2023-06-02 12:27:56 +0200

    feat: add products json.

--- products-service/lib/products-service-stack.ts
+++ products-service/lib/products-service-stack.ts
@@ -1,10 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ProductsServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new lambda.Function(this, 'ProductsServiceAPI', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'get-products.handler'
+    })
 
     // The code that defines your stack goes here
 
