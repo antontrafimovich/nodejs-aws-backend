93cefffd7e26af5297efa1c818a88d291f9c2f2e
Anton Trafimovich <tonytrof@gmail.com>
2023-06-03 15:28:53 +0200

    feat: add api gateway.

--- products-service/lib/products-service-stack.ts
+++ products-service/lib/products-service-stack.ts
@@ -1,17 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ProductsServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'ProductsServiceAPI', {
+
+    const getProductsLambda = new lambda.Function(this, 'ProductsServiceAPI', {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'get-products.handler'
     })
+
+    const productsIntegration = new HttpLambdaIntegration('ProductsIntegration', getProductsLambda);
+
+    const http = new apigateway.HttpApi(this, 'ProductsServiceHTTP');
+
+    http.addRoutes({
+      path: '/products',
+      integration: productsIntegration,
+      methods: [apigateway.HttpMethod.GET]
+    })
+
 
     // The code that defines your stack goes here
 
