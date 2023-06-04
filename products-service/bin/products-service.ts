2f88706ae00e362860d5f9829726a94b67e1c9f3
Anton Trafimovich <tonytrof@gmail.com>
2023-06-04 23:14:24 +0200

    feat: add .env with region and account info and used it in stack registration.

--- products-service/bin/products-service.ts
+++ products-service/bin/products-service.ts
@@ -3,8 +3,14 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ProductsServiceStack } from '../lib/products-service-stack';
 
+import 'dotenv/config';
+
 const app = new cdk.App();
 new ProductsServiceStack(app, 'ProductsServiceStack', {
+  env: {
+    account: process.env.AWS_ACCOUNT,
+    region: process.env.AWS_REGION
+  }
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
