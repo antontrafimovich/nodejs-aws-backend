0a0f82e3880d47bab33d0a77ad3dbdb0f70bf444
Anton Trafimovich <tonytrof@gmail.com>
2023-06-08 18:12:17 +0200

    feat: add error handling to createProducts lambda.

--- products-service/lambda/createProduct.ts
+++ products-service/lambda/createProduct.ts
@@ -7,7 +7,7 @@ const ddb = new DynamoDBClient({ region: process.env.REGION });
 
 const put = (item: Omit<Product, "id">) => {
   const putCommand = new PutItemCommand({
-    TableName: process.env.TABLE_NAME as string,
+    TableName: process.env.PRODUCTS_TABLE_NAME as string,
     Item: {
       id: {
         S: uid(5),
@@ -27,28 +27,50 @@ const put = (item: Omit<Product, "id">) => {
   return ddb.send(putCommand);
 };
 
+const validateInputProduct = (item: Record<string, any>): void => {
+  if (!item.title) {
+    throw new Error("Product title can't be empty");
+  }
+};
+
 export const handler = async (event: any) => {
+  console.log(`POST /products ${event.body}`);
+
   let item;
 
   try {
     item = JSON.parse(event.body);
-  } catch (err) {
+  } catch (err: any) {
     return {
       statusCode: 500,
-      headers: { "Content-Type": "application/json" },
-      body: err,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message,
+    };
+  }
+
+  try {
+    validateInputProduct(item);
+  } catch (err) {
+    return {
+      statusCode: 400,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify((err as Error).message),
     };
   }
 
   let putResult;
 
   try {
-    putResult = await put(item);
-  } catch (err) {
+    putResult = await put({
+      title: item.title,
+      description: item.description,
+      price: item.price,
+    });
+  } catch (err: any) {
     return {
       statusCode: 500,
-      headers: { "Content-Type": "application/json" },
-      body: err,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify(err.$metadata.message),
     };
   }
 
