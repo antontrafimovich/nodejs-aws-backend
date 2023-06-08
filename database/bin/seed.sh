86145de49287a0eccb19d9c12dc5c22b7e40b240
Anton Trafimovich <tonytrof@gmail.com>
2023-06-08 16:54:03 +0200

    feat: add ability to seed stocks.

--- database/bin/seed.sh
+++ database/bin/seed.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# aws --region eu-north-1 dynamodb batch-write-item \
+#     --request-items file://products.json \
+#     --return-consumed-capacity INDEXES \
+#     --return-item-collection-metrics SIZE
+
 aws --region eu-north-1 dynamodb batch-write-item \
-    --request-items file://seed.json \
+    --request-items file://stocks.json \
     --return-consumed-capacity INDEXES \
     --return-item-collection-metrics SIZE
