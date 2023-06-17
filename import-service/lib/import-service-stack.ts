a28462c0a6346626759aa3399a35281cbacb9caf
Anton Trafimovich <tonytrof@gmail.com>
2023-06-17 20:54:58 +0200

    feat: add api gateway with import route.

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigateway from "@aws-cdk/aws-apigatewayv2-alpha";
+
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import path = require("path");
+import {
+  MappingValue,
+  ParameterMapping,
+} from "@aws-cdk/aws-apigatewayv2-alpha";
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -22,6 +29,29 @@ export class ImportServiceStack extends cdk.Stack {
 
     const bucket = new Bucket(this, "ImportCSVBucket");
     bucket.grantWrite(importCSVLambda);
+
+    const http = new apigateway.HttpApi(this, "ProductsServiceHTTP", {
+      corsPreflight: {
+        allowOrigins: ["*"],
+        allowMethods: [apigateway.CorsHttpMethod.ANY],
+      },
+    });
+
+    const importCSVIntegration = new HttpLambdaIntegration(
+      "importCSVIntegration",
+      importCSVLambda,
+      {
+        parameterMapping: ParameterMapping.fromObject({
+          name: MappingValue.requestQueryString("name"),
+        }),
+      }
+    );
+
+    http.addRoutes({
+      path: "/import",
+      integration: importCSVIntegration,
+      methods: [apigateway.HttpMethod.POST],
+    });
 
     // The code that defines your stack goes here
 
