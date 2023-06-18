44154353128f7309822a8dd81874ea513bd76891
Anton Trafimovich <tonytrof@gmail.com>
2023-06-18 20:26:20 +0200

    fix: get rid of unused imports.

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,11 @@
+import * as apigateway from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import * as apigateway from "@aws-cdk/aws-apigatewayv2-alpha";
-
-import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 import { Bucket, HttpMethods } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import path = require("path");
-import {
-  MappingValue,
-  ParameterMapping,
-} from "@aws-cdk/aws-apigatewayv2-alpha";
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
