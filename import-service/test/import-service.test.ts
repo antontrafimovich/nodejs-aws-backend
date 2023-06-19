70413ad1200c3cda8134d7b545b620a75869b6fc
Anton Trafimovich <tonytrof@gmail.com>
2023-06-20 00:01:21 +0200

    feat: add tests for importProductsFile module.

--- import-service/test/import-service.test.ts
+++ import-service/test/import-service.test.ts
@@ -1,17 +1,85 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as ImportService from '../lib/import-service-stack';
+jest.mock("@aws-sdk/s3-request-presigner", () => {
+  const original = jest.requireActual("@aws-sdk/s3-request-presigner");
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/import-service-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new ImportService.ImportServiceStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  return {
+    __esModule: true,
+    ...original,
+    getSignedUrl: jest.fn(() => "some url"),
+  };
+});
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+jest.mock("@aws-sdk/client-s3", () => {
+  const original = jest.requireActual("@aws-sdk/client-s3");
+
+  return {
+    __esModule: true,
+    ...original,
+    S3Client: class S3TestClient {
+      constructor(private params: Record<string, any>) {}
+    },
+    PutObjectCommand: class PutObjectTestCommand {
+      constructor(private params: Record<string, any>) {}
+    },
+  };
+});
+
+import { handler as importProductsFileHandler } from "../lambda/importProductsFile";
+
+describe("importProductsFile", () => {
+  it("should return error if name query param is not provided", async () => {
+    const result = await importProductsFileHandler({
+      queryStringParameters: {
+        name: undefined,
+      },
+    });
+
+    expect(result.statusCode).toEqual(400);
+    expect(result.body).toEqual("Filename hasn't been provided");
+  });
+
+  it("should return error if name REGION env variable is not provided", async () => {
+    const result = await importProductsFileHandler({
+      queryStringParameters: {
+        name: "test_name",
+      },
+    });
+
+    expect(result.statusCode).toEqual(500);
+    expect(result.body).toEqual("region env var hasn't been provided");
+  });
+
+  it("should return error if BUCKET_NAME env variable is not provided", async () => {
+    process.env.REGION = "test_region";
+
+    const result = await importProductsFileHandler({
+      queryStringParameters: {
+        name: "test_name",
+      },
+    });
+
+    expect(result.statusCode).toEqual(500);
+    expect(result.body).toEqual("bucket env var hasn't been provided");
+  });
+
+  it("should return signedUrl response if query name and all the required env variables are provided", async () => {
+    process.env.REGION = "test_region";
+    process.env.BUCKET_NAME = "test_bucket_name";
+
+    const result = await importProductsFileHandler({
+      queryStringParameters: {
+        name: "test_name",
+      },
+    });
+
+    expect(result.statusCode).toEqual(200);
+    expect(result.body).toEqual("some url");
+  });
+
+  afterAll(() => {
+    jest.unmock("@aws-sdk/s3-request-presigner");
+    jest.unmock("@aws-sdk/client-s3");
+  });
 });
