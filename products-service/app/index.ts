9a74d74abd7aa894311e5fa088524d9d36516f38
Anton Trafimovich <tonytrof@gmail.com>
2023-06-25 17:35:30 +0200

    feat: add repo and db initializators to app.

---
+++ products-service/app/index.ts
@@ -0,0 +1,14 @@
+import { DynamoDB } from "../db";
+import { ProductsRepo, StocksRepo } from "../repo";
+
+const db = new DynamoDB();
+
+export const productsRepo = new ProductsRepo(
+  db,
+  process.env.PRODUCTS_TABLE_NAME as string
+);
+
+export const stocksRepo = new StocksRepo(
+  db,
+  process.env.STOCKS_TABLE_NAME as string
+);
