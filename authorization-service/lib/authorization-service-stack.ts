9f77e67ad12b7177d11faf3888975a1f0634df1e
Anton Trafimovich <tonytrof@gmail.com>
2023-06-28 13:43:36 +0200

    feat: add scaffold for basicAuthorizer handler.

--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,16 +1,31 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import path from "node:path";
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthorizationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new NodejsFunction(
+      this,
+      "basicAuthorizationHandler",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: path.join(
+          __dirname,
+          "../",
+          "src",
+          "handlers",
+          "basicAuthorizer.ts"
+        ),
+        handler: "handler",
+        environment: {
+          GITHUB_CREDS: process.env.GITHUB_CREDS as string,
+          REGION: process.env.AWS_REGION as string,
+        },
+      }
+    );
   }
 }
