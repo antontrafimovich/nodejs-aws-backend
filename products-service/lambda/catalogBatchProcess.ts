eed8be18ea1229066628a532e07d37425c11712e
Anton Trafimovich <tonytrof@gmail.com>
2023-07-27 20:19:04 +0200

    feat: replace uid with uuid.

--- products-service/lambda/catalogBatchProcess.ts
+++ products-service/lambda/catalogBatchProcess.ts
@@ -1,5 +1,5 @@
 import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
-import { uid } from "uid";
+import { v4 as uuidv4 } from "uuid";
 
 import { productsRepo, stocksRepo } from "../app";
 import { getRandomPhotoUrl } from "../shared";
@@ -11,7 +11,7 @@ export const handler = async (event: any) => {
     const promises = event.Records.map(async (record: any) => {
       const product = JSON.parse(record.body);
 
-      const id = uid(5);
+      const id = uuidv4();
       const image = await getRandomPhotoUrl();
 
       await Promise.all([
